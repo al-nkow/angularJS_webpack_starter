668cefd6f9d61b787d9e892f189c24e6740304ea
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-01-08 20:23:16 +0300

    material

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,19 @@
-import angular from 'angular';
-import uirouter from '@uirouter/angularjs';
+import angular    from 'angular';
+import uirouter   from '@uirouter/angularjs';
+import ngMaterial from 'angular-material';
+import ngMessages from 'angular-messages';
+import ngAnimate  from 'angular-animate';
 
 import '../style/app.css';
-
-import routing from '../app.config';
+import routing from './app.config';
 import contacts from '../modules/contacts';
 
 var myApp = angular.module('myApp', [
     uirouter,
-    contacts
-    // 'ngMaterial',
-    // 'ngMessages',
-    // 'ngAnimate',
+    contacts,
+    ngMaterial,
+    ngMessages,
+    ngAnimate,
     // 'myApp.home',
     // 'myApp.about',
     // 'myApp.contacts',
@@ -19,20 +21,4 @@ var myApp = angular.module('myApp', [
 ]);
 
 myApp.config(routing);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default myApp;
